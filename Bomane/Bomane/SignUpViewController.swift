ab98eec8cbb9ead70a0a6c33fd094d7abf23226b
Jeremy Sharvit <sharvitjeremy@gmail.com>
2016-11-27 11:28:48 -0500

    initial commit

---
+++ Bomane/Bomane/SignUpViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SignUpViewController.swift
+//  Bomane
+//
+//  Created by Jeremy Sharvit on 2016-11-27.
+//  Copyright © 2016 com.bomane. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
