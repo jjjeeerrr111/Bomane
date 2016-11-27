596b4c0b2cb7ad5f51adf3bbcf566a269b51998f
Jeremy Sharvit <sharvitjeremy@gmail.com>
2016-11-27 12:07:15 -0500

    adjust title positioning for sign in buttons

--- Bomane/Bomane/SignUpViewController.swift
+++ Bomane/Bomane/SignUpViewController.swift
@@ -10,8 +10,15 @@ import UIKit
 
 class SignUpViewController: UIViewController {
 
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var facebookButton: UIButton!
+    @IBOutlet weak var createAccountButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        createAccountButton.contentHorizontalAlignment = .left
+        facebookButton.contentHorizontalAlignment = .left
+        createAccountButton.titleEdgeInsets = UIEdgeInsetsMake(0, 10, 0, 0)
+        facebookButton.titleEdgeInsets = UIEdgeInsetsMake(0, 10, 0, 0)
 
     }
 
@@ -20,5 +27,15 @@ class SignUpViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func loginButtonPressed(_ sender: UIButton) {
+        print("login pressed")
+    }
+    
+    @IBAction func createAccountButtonPressed(_ sender: UIButton) {
+        print("create pressed")
+    }
 
+    @IBAction func facebookButtonPressed(_ sender: UIButton) {
+        print("facebook pressed")
+    }
 }
