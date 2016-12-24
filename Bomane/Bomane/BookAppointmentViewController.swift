fcd7cfb6203383d5573d4b5ef924a7578ef11935
Jeremy Sharvit <sharvitjeremy@gmail.com>
2016-12-24 16:03:26 -0500

    added the book appointment view, started the ui, added the calendar pod

---
+++ Bomane/Bomane/BookAppointmentViewController.swift
@@ -0,0 +1,66 @@
+//
+//  BookAppointmentViewController.swift
+//  Bomane
+//
+//  Created by Jeremy Sharvit on 2016-12-24.
+//  Copyright © 2016 com.bomane. All rights reserved.
+//
+
+import UIKit
+
+class BookAppointmentViewController: UIViewController {
+    
+    static let shared = BookAppointmentViewController()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.white
+        setUpNavBar()
+        // Do any additional setup after loading the view.
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        navigationController?.navigationBar.barTintColor = UIColor.white
+        navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName:UIColor.black, NSFontAttributeName:UIFont(name: "Baskerville", size: 20)!]
+        navigationController?.navigationBar.isTranslucent = false
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .default
+    }
+    
+    func setUpNavBar() {
+        navigationController?.navigationBar.barTintColor = UIColor.white
+        navigationItem.title = "BOOK APPOINTMENT"
+        navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName:UIColor.black, NSFontAttributeName:UIFont(name: "Baskerville", size: 20)!]
+        navigationController?.navigationBar.isTranslucent = false
+        
+        //This line sets the back button title to "" so that it doesnt show up at < Messages when pushing
+        //the next view controller
+        self.navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: UIBarButtonItemStyle.plain, target: nil, action: nil)
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "hamburgerIcon"), style: .plain, target: self, action: #selector(self.menuButtonPressed(sender:)))
+        self.navigationItem.leftBarButtonItem?.tintColor = UIColor.black
+        
+    }
+    
+    func menuButtonPressed(sender: UIBarButtonItem) {
+        let menu = ScreenMenuViewController.shared
+        let navVC = UINavigationController(rootViewController: menu)
+        navVC.transitioningDelegate = self
+        navVC.modalPresentationStyle = .overFullScreen
+        self.present(navVC, animated: true, completion: nil)
+    }
+}
+
+extension BookAppointmentViewController:UIViewControllerTransitioningDelegate {
+    func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        let animator = SlideAnimator()
+        animator.presenting = false
+        return animator
+    }
+    
+    func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return SlideAnimator()
+    }
+}
